2ea1d044df22778b94d9ae7f85c93471067914a7
krishgalam <72341127+krishgalam@users.noreply.github.com>
2021-06-24 08:58:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "azurerm" { 
     version = "2.20.1"
-    subscription_id = "36fa8384-fff9-42f4-afcf-52fce4d6736e"
+    subscription_id = ""
     client_id = ""
     tenant_id = ""
     features {
